7839a42949104ab278a53f7b0911b0cf41cbad80
Maxwell Johnson <maximusjohnsonus@gmail.com>
2019-02-08 22:36:13 -0500

    Added selection ability

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import sys, math
 from enum import Enum
 from PySide2 import QtCore, QtGui, QtWidgets
-from PySide2.QtWidgets import (QApplication, QWidget)
+from PySide2.QtCore import Qt
+from PySide2.QtWidgets import QApplication, QWidget
+from PySide2.QtGui import QPen, QPainter
 
 class BaseObject():
     def __init__(self):
         self.pos = (200,200)
         self.size = (100,100)
         self.label = None
+        self.selected = False
 
     def in_bounds(self, pos):
         return False
 
     def draw(self, painter):
         pass
+
+    def select(self):
+        self.selected = not self.selected
 
 class PortType(Enum):
     UNDEF = -1
@@ -38,6 +44,11 @@ class Module(BaseObject):
                                  self.size[0], self.size[1])
 
     def draw(self, painter):
+        pen = QPen(Qt.black, 2, Qt.SolidLine)
+        if self.selected:
+            pen = QPen(Qt.green, 2, Qt.SolidLine)
+
+        painter.setPen(pen)
         painter.drawRect(self.rect)
 
     def in_bounds(self, pos):
@@ -63,8 +74,8 @@ class Canvas(QWidget):
         self.objects = [test_module]
 
     def paintEvent(self, event):
-        pen = QtGui.QPen()
-        painter = QtGui.QPainter()
+        pen = QPen()
+        painter = QPainter()
         painter.begin(self)
         painter.setPen(pen)
 
@@ -77,7 +88,10 @@ class Canvas(QWidget):
         pos = (event.pos().x(), event.pos().y())
         for o in self.objects:
             if o.in_bounds(pos):
+                o.select()
                 print("clicked:", o)
+
+        self.update()
 
 
 
